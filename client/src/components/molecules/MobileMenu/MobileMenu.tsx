7a25ca121d6987336095891485c9d17ff8a91e08
Jakub Gryzinski <jakubgryzinski@MacBook-Pro-Jakub.local>
2022-01-03 18:11:48 +0100

    repair focus bug

--- client/src/components/molecules/MobileMenu/MobileMenu.tsx
+++ client/src/components/molecules/MobileMenu/MobileMenu.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import MenuContext from '../../../context/MenuContext';
-import OutsideClickHandle from '../../../hooks/OutsideClickHandle';
 import MenuButton from '../../atoms/MenuButton/MenuButton';
 import MobileMenuList from '../../atoms/MobileMenuList/MobileMenuList';
 
 function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
   const setMenuOpen = () => setIsOpen(!isOpen);
-  const ref = OutsideClickHandle(setIsOpen);
   return (
     <MenuContext.Provider
       value={{
@@ -15,7 +13,7 @@ function MobileMenu() {
         setMenuOpen,
       }}
     >
-      <div ref={ref}>
+      <div>
         <div className="relative z-20">
           <MenuButton menuContext={isOpen} />
         </div>
